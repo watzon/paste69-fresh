b6dc364d14878a34ef3dbe017992dc9d5631ddc9
Chris W <cawatson1993@gmail.com>
2023-09-27 20:52:57 -0600

    simulate disabled with anchor button

--- components/Button.tsx
+++ components/Button.tsx
@@ -12,7 +12,7 @@ export default function Button(
         ? (
           <a
             {...props as JSX.HTMLAttributes<HTMLAnchorElement>}
-            class="px-2 py-2 text-gray-100 border border-gray-300 bg-teal-500 hover:bg-teal-800 disabled:bg-gray-500 bg-opacity-30 transition-colors"
+            class={`px-2 py-2 text-gray-100 border border-gray-300 hover:bg-teal-800 bg-opacity-30 transition-colors ${props.disabled ? "bg-gray-500 pointer-events-none" : "bg-teal-500"}`}
           />
         )
         : (
