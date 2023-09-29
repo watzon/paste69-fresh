9f2ed89863c138a58fb7a41630424943505cda21
Chris W <cawatson1993@gmail.com>
2023-09-29 10:02:50 -0600

    update about

--- routes/about.tsx
+++ routes/about.tsx
@@ -1,4 +1,9 @@
 import ToolBox from "../islands/ToolBox.tsx";
+import IconDeviceFloppy from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/device-floppy.tsx";
+import IconTextPlus from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/text-plus.tsx";
+import IconCopy from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/copy.tsx";
+import IconMoon from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/moon.tsx";
+import IconSun from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/sun.tsx";
 
 export default function About() {
   return (
@@ -16,6 +21,26 @@ export default function About() {
           easy to use pastebin service based on HasteBin. Like HasteBin, it's
           also open source and can be found over on{" "}
           <a href="https://github.com/watzon/paste69-fresh">GitHub</a>.
+        </p>
+
+        <h2 class="mt-4 mb-2">Usage</h2>
+
+        <p>
+          To create a paste, go <a href="/">home</a> or click the "New" button (<IconTextPlus class="inline-block w-4 h-4" />)
+          in the tool box in the bottom right corner of the page. Paste whatever text you want into the editor,
+          and click the "Save" button (<IconDeviceFloppy class="inline-block w-4 h-4" />) to create the paste.
+        </p>
+
+        <p>
+          To copy an existing paste, click the "Copy" button (<IconCopy class="inline-block w-4 h-4" />)
+          in the tool box in the bottom right corner of the page. This will start a new paste with
+          the contents of the existing paste.
+        </p>
+
+        <p>
+          Lastly, use the "Toggle theme" button (<IconMoon class="inline-block w-4 h-4" />/<IconSun class="inline-block w-4 h-4" />)
+          to toggle between light and dark mode. The theme you choose will be saved in your browser's
+          local storage, and loaded the next time you visit the site.
         </p>
 
         <h2 class="mt-4 mb-2">API</h2>
@@ -47,11 +72,11 @@ export default function About() {
         <pre>
                     <code>
                         {`{
-        "id": "paste id",
-        "contents": "paste contents",
-        "views": 0,
-        "created_at": "2021-08-05T07:30:00.000Z",
-    }`}
+    "id": "paste id",
+    "contents": "paste contents",
+    "views": 0,
+    "created_at": "2021-08-05T07:30:00.000Z",
+}`}
                     </code>
         </pre>
 
@@ -66,11 +91,11 @@ export default function About() {
         <pre>
                     <code>
                         {`{
-        "id": "paste id",
-        "contents": "paste contents",
-        "views": 0,
-        "created_at": "2021-08-05T07:30:00.000Z",
-    }`}
+    "id": "paste id",
+    "contents": "paste contents",
+    "views": 0,
+    "created_at": "2021-08-05T07:30:00.000Z",
+}`}
                     </code>
         </pre>
       </div>
